5171ca5887cc7cb6f2c9e8e0cfb56bd21d6cd0b9
Santiago Arellano <santiagoarellano703@gmail.com>
2024-06-14 22:03:56 -0400

    feat: add styles at home, login and signup

--- money_manager/settings.py
+++ money_manager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "finance" / "static",]
 
 LOGIN_URL = '/signin'
 
